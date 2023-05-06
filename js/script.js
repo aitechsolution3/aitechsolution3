84bab04425e2708962d30f10abb55887fc8379ff
aitechsolution3 <132743037+aitechsolution3@users.noreply.github.com>
2023-05-06 14:26:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*-----typing animation ------*/
+var typed= new Typed(".typing1", {
+    strings:[" ","Solutions","SOLUTIONS"],
+    typeSpeed:500,
+    BackSpeed:10,
+    loop:true
+
+})
+var typed= new Typed(".typing", {
+    strings:[" ","Easier","easier"],
+    typeSpeed:400,
+    BackSpeed:60,
+    loop:true
+
+})
